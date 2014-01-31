38f1135b8f94f0881a25f95e3153782c9f985f4f
Martin Braun <martinbraun123@aol.com>
2014-01-31 23:37:37 +0100

    added licenses

--- src/test/java/de/hotware/hibernate/query/SearcherTest.java
+++ src/test/java/de/hotware/hibernate/query/SearcherTest.java
@@ -1,3 +1,19 @@
+/*  
+ * This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.*
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *   
+ *   (C) Martin Braun 2014
+ */
 package de.hotware.hibernate.query;
 
 import static org.junit.Assert.assertTrue;
@@ -104,8 +120,8 @@ public class SearcherTest {
 			placeQuery.setName("Valinor");
 			@SuppressWarnings("unchecked")
 			List<Place> list = this.multiClassSearcher
-			.search(placeQuery, session, Place.class,
-					PlaceQueryBean.class).getFullTextQuery().list();
+					.search(placeQuery, session, Place.class,
+							PlaceQueryBean.class).getFullTextQuery().list();
 			assertTrue(list.size() > 0);
 			System.out.println(list);
 		} finally {
